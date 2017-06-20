c305e548d2997c8296e733db71e4d403aa74881f
octavio <ocubillosj@gmail.com>
2017-06-20 09:39:45 -0400

    tabla tradicional

--- proyecto/wsgi.py
+++ proyecto/wsgi.py
@@ -15,7 +15,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prevextaller.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
